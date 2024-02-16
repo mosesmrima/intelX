73aef4d5f6716fbe742ead591399acb2ea3fba61
mrima <mrimamss@gmail.com>
2024-02-16 10:45:36 +0300

    monti ransomware parser

--- src/scraper/parsers/monti.js
+++ src/scraper/parsers/monti.js
@@ -1,29 +1,17 @@
-const fs = require('fs');
 const cheerio = require('cheerio');
 
-function main() {
-    let list_div = [];
+function parseHTMLContent(htmlContent) {
+    let listDiv = [];
+    const $ = cheerio.load(htmlContent);
 
-    fs.readdirSync('source').forEach(filename => {
-        try {
-            if (filename.startsWith(__filename.split('.')[0]+'-')) {
-                let html_doc = 'source/'+filename;
-                let file = fs.readFileSync(html_doc, 'utf8');
-                let $ = cheerio.load(file);
-                let divs_name = $('a.leak-card.p-3');
-                divs_name.each((i, div) => {
-                    let title = $(div).find('h5').text().trim();
-                    let description = $(div).find('p').text().trim();
-                    let link = $(div).attr('href');
-                    list_div.push({'title':title, 'description': description, "link": link, "slug": filename});
-                });
-            }
-        } catch (error) {
-            console.log("Failed during : " + filename);
-        }
+    $('a.leak-card.p-3').each((i, div) => {
+        let title = $(div).find('h5').text().trim();
+        let description = $(div).find('p').text().trim();
+        let link = $(div).attr('href');
+        listDiv.push({'title': title, 'description': description, "link": link});
     });
-    console.log(list_div);
-    return list_div;
+
+    return listDiv;
 }
 
-main();
+module.exports = parseHTMLContent;
