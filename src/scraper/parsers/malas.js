31095769e77078e95fa4338af2de1dc468e46667
mrima <mrimamss@gmail.com>
2024-02-16 10:44:02 +0300

    malas ransomware parser

--- src/scraper/parsers/malas.js
+++ src/scraper/parsers/malas.js
@@ -1,40 +1,26 @@
-const fs = require('fs');
 const cheerio = require('cheerio');
-const path = require('path');
 
-function main() {
-    let list_div = [];
-
-    fs.readdirSync('source').forEach(filename => {
-        try {
-            if (filename.startsWith(path.basename(__filename, '.js') + '-')) {
-                let html_doc = 'source/' + filename;
-                let file = fs.readFileSync(html_doc, 'utf8');
-                let $ = cheerio.load(file);
-                if (filename.endsWith('xml.html')) {
-                    let items = $('item');
-                    items.each((i, item) => {
-                        let title = $(item).find('title').text();
-                        let description = $(item).find('description').text();
-                        list_div.push({"title" : title, "description" : description});
-                    });
-                } else {
-                    let liste = $('ul.list');
-                    let divs_name = liste.find('li');
-                    divs_name.each((i, div) => {
-                        let title = $(div).find('a').attr('title').trim();
-                        let description = "";
-                        let link = $(div).find('a').attr('href');
-                        list_div.push({"title" : title, "description" : description, "link": link, "slug": filename});
-                    });
-                }
-            }
-        } catch (err) {
-            console.log("Failed during : " + filename);
-        }
+function parseHTMLContent(htmlContent, isXML) {
+    let listDiv = [];
+    const $ = cheerio.load(htmlContent, {
+        xmlMode: isXML // Process as XML if isXML is true
     });
-    console.log(list_div);
-    return list_div;
+
+    if (isXML) {
+        $('item').each((i, item) => {
+            const title = $(item).find('title').text();
+            const description = $(item).find('description').text();
+            listDiv.push({ "title": title, "description": description });
+        });
+    } else {
+        $('ul.list > li').each((i, li) => {
+            const title = $(li).find('a').attr('title').trim();
+            const link = $(li).find('a').attr('href');
+            listDiv.push({ "title": title, "description": "", "link": link });
+        });
+    }
+
+    return listDiv;
 }
 
-main();
+module.exports = parseHTMLContent;
