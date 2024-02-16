cfee20fa570a16298b968c951bb62e3fd282534f
mrima <mrimamss@gmail.com>
2024-02-16 13:34:25 +0300

    royal ransomware parser

--- src/scraper/parsers/royal.js
+++ src/scraper/parsers/royal.js
@@ -1,34 +1,21 @@
-const fs = require('fs');
-const path = require('path');
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+const cheerio = require('cheerio');
 
-async function main() {
-    let list_div = [];
+function parseHTMLContent(htmlContent) {
+    let listDiv = [];
+    const $ = cheerio.load(htmlContent);
 
-    const files = fs.readdirSync('source');
-    for (let i = 0; i < files.length; i++) {
-        const filename = files[i];
-        if (filename.startsWith(path.basename(__filename).split('.')[0]+'-')) {
-            const html_doc = 'source/'+filename;
-            const data = fs.readFileSync(html_doc, 'utf8');
-            const dom = new JSDOM(data);
-            const document = dom.window.document;
-            const divs_name = document.querySelectorAll('div.card');
-            divs_name.forEach(div => {
-                try {
-                    const title = div.querySelector('h2').textContent.trim();
-                    const description = div.querySelector('main').textContent.trim();
-                    const link = div.nextElementSibling.querySelector('a').href;
-                    list_div.push({"title" : title, "description" : description, "link": link, "slug": filename});
-                } catch (error) {
-                    console.log("Failed during : " + filename);
-                }
-            });
+    $('div.card').each((i, div) => {
+        try {
+            const title = $(div).find('h2').text().trim();
+            const description = $(div).find('main').text().trim();
+            const link = $(div).next('div').find('a').attr('href');
+            listDiv.push({ "title": title, "description": description, "link": link });
+        } catch (error) {
+            console.error("Error processing a div.card:", error);
         }
-    }
-    console.log(list_div);
-    return list_div;
+    });
+
+    return listDiv;
 }
 
-main();
+module.exports = parseHTMLContent;
