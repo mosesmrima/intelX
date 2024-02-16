4aada5d3f2225ef5438f598c668a2c2bf3552217
mrima <mrimamss@gmail.com>
2024-02-16 13:33:45 +0300

    suncrypt ransomware parser

--- src/scraper/parsers/suncrypt.js
+++ src/scraper/parsers/suncrypt.js
@@ -1,27 +1,17 @@
-const fs = require('fs');
-const path = require('path');
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+const cheerio = require('cheerio');
 
-async function main() {
-    let list_div = [];
+function parseHTMLContent(htmlContent) {
+    let listDiv = [];
+    const $ = cheerio.load(htmlContent);
 
-    const files = fs.readdirSync('source');
-    for (let i = 0; i < files.length; i++) {
-        const filename = files[i];
-        if (filename.startsWith(path.basename(__filename, '.js') + '-')) {
-            const html_doc = path.join('source', filename);
-            const file = fs.readFileSync(html_doc, 'utf-8');
-            const dom = new JSDOM(file);
-            const divs_name = dom.window.document.querySelectorAll('div.title.is-4');
-            for (let div of divs_name) {
-                list_div.push(div.querySelector('a').textContent.trim());
-            }
+    $('div.title.is-4').each((i, div) => {
+        const title = $(div).find('a').text().trim();
+        if (title) {
+            listDiv.push(title);
         }
-    }
-    list_div = [...new Set(list_div)]; // remove duplicates
-    console.log(list_div);
-    return list_div;
+    });
+
+    return [...new Set(listDiv)];
 }
 
-main();
+module.exports = parseHTMLContent;
