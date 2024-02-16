3736134322c1f132658f0f27921d1fc153a580b7
mrima <mrimamss@gmail.com>
2024-02-16 13:34:13 +0300

    snatch ransomware parser

--- src/scraper/parsers/snatch.js
+++ src/scraper/parsers/snatch.js
@@ -1,29 +1,22 @@
-const fs = require('fs');
 const cheerio = require('cheerio');
 
-function main() {
-    let list_div = [];
+function parseHTMLContent(htmlContent) {
+    let listDiv = [];
+    const $ = cheerio.load(htmlContent);
 
-    fs.readdirSync('source').forEach(filename => {
-        try {
-            if (filename.startsWith(__filename.split('.')[0]+'-')) {
-                let html_doc = 'source/'+filename;
-                let file = fs.readFileSync(html_doc, 'utf8');
-                let $ = cheerio.load(file);
-                let divs_name = $('div.ann-block');
-                divs_name.each((i, div) => {
-                    let title = $(div).find('div.a-b-n-name').text().trim();
-                    let description = $(div).find('div.a-b-text').text().trim();
-                    let link = $(div).find('button').attr('onclick').split("'")[1];
-                    list_div.push({'title':title, 'description': description, 'link':link, 'slug': filename});
-                });
-            }
-        } catch (error) {
-            console.log("Failed during : " + filename);
+    $('div.ann-block').each((i, div) => {
+        let title = $(div).find('div.a-b-n-name').text().trim();
+        let description = $(div).find('div.a-b-text').text().trim();
+        let onclickValue = $(div).find('button').attr('onclick');
+        let link = '';
+        if (onclickValue) {
+            const linkMatch = onclickValue.match(/'([^']+)'/);
+            link = linkMatch ? linkMatch[1] : '';
         }
+        listDiv.push({ 'title': title, 'description': description, 'link': link });
     });
-    console.log(list_div);
-    return list_div;
+
+    return listDiv;
 }
 
-main();
+module.exports = parseHTMLContent;
