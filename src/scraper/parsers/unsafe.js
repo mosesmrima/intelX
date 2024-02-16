1383bf248b8fb960bba33a80e47a80504311095c
mrima <mrimamss@gmail.com>
2024-02-16 13:33:24 +0300

    unsafe ransomware parser

--- src/scraper/parsers/unsafe.js
+++ src/scraper/parsers/unsafe.js
@@ -1,33 +1,20 @@
-const fs = require('fs');
-const path = require('path');
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+const cheerio = require('cheerio');
 
-async function main() {
-    let list_div = [];
+function parseHTMLContent(htmlContent) {
+    let listDiv = [];
+    const $ = cheerio.load(htmlContent);
 
-    const files = fs.readdirSync('source');
-    for (let i = 0; i < files.length; i++) {
-        const filename = files[i];
-        if (filename.startsWith(path.basename(__filename).split('.')[0]+'-')) {
-            const html_doc = 'source/'+filename;
-            const data = fs.readFileSync(html_doc, 'utf8');
-            const dom = new JSDOM(data);
-            const document = dom.window.document;
-            const divs_name = document.querySelectorAll('div.blog-card');
-            divs_name.forEach(div => {
-                try {
-                    const title = div.querySelector('div h4').textContent.trim();
-                    const description = "";
-                    list_div.push({"title" : title, "description" : description});
-                } catch (error) {
-                    console.log("Failed during : " + filename);
-                }
-            });
+    $('div.blog-card').each((i, div) => {
+        try {
+            const title = $(div).find('div h4').text().trim();
+            const description = "";
+            listDiv.push({ "title": title, "description": description });
+        } catch (error) {
+            console.log(error)
         }
-    }
-    console.log(list_div);
-    return list_div;
+    });
+
+    return listDiv;
 }
 
-main();
+module.exports = parseHTMLContent;
