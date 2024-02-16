11f6725f4b2fdfb219ef4510ee636977f40bb145
mrima <mrimamss@gmail.com>
2024-02-16 10:47:10 +0300

    ransomed  ransomware parser

--- src/scraper/parsers/ransomed.js
+++ src/scraper/parsers/ransomed.js
@@ -1,31 +1,19 @@
-const fs = require('fs');
 const cheerio = require('cheerio');
 
-function main() {
-    let list_div = [];
+function parseHTMLContent(htmlContent) {
+    let listDiv = [];
+    const $ = cheerio.load(htmlContent);
 
-    fs.readdirSync('source').forEach(filename => {
-        if (filename.startsWith(__filename.split('.')[0]+'-')) {
-            try {
-                let html_doc = 'source/'+filename;
-                console.log(filename);
-                let file = fs.readFileSync(html_doc, 'utf8');
-                let $ = cheerio.load(file);
-                let divs_name = $('li.wp-block-post');
-                divs_name.each((i, div) => {
-                    let meta = $(div).find('a');
-                    let title = meta.text().trim();
-                    let description = $(div).find('div.wp-block-post-excerpt').text().trim();
-                    let link = meta.attr('href');
-                    list_div.push({"title": title, "description": description, "link": link, "slug": filename});
-                });
-            } catch (error) {
-                // pass
-            }
-        }
+    $('li.wp-block-post').each((i, div) => {
+        const meta = $(div).find('a');
+        const title = meta.text().trim();
+        const description = $(div).find('div.wp-block-post-excerpt').text().trim();
+        const link = meta.attr('href');
+        listDiv.push({ "title": title, "description": description, "link": link });
     });
-    console.log(list_div);
-    return list_div;
+
+    return listDiv;
 }
 
-main();
+module.exports = parseHTMLContent;
+
