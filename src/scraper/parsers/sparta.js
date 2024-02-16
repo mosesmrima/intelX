470114b59a2cd45ddde73cd08f3af24f38c7c4af
mrima <mrimamss@gmail.com>
2024-02-16 13:34:05 +0300

    sparta ransomware parser

--- src/scraper/parsers/sparta.js
+++ src/scraper/parsers/sparta.js
@@ -1,28 +1,16 @@
-const fs = require('fs');
 const cheerio = require('cheerio');
 
-function main() {
+function parseHTMLContent(htmlContent) {
     let listDiv = [];
+    const $ = cheerio.load(htmlContent);
 
-    fs.readdirSync('source').forEach(filename => {
-        try {
-            if (filename.startsWith(__filename.split('.')[0] + '-')) {
-                let htmlDoc = 'source/' + filename;
-                let file = fs.readFileSync(htmlDoc, 'utf8');
-                let $ = cheerio.load(file);
-                let divsName = $('div.card.mb-2');
-                divsName.each((i, div) => {
-                    let title = $(div).find('h2').text().trim();
-                    let description = $(div).find('p').text().trim();
-                    listDiv.push({ 'title': title, 'description': description });
-                });
-            }
-        } catch (error) {
-            console.log("Failed during : " + filename);
-        }
+    $('div.card.mb-2').each((i, div) => {
+        let title = $(div).find('h2').text().trim();
+        let description = $(div).find('p').text().trim();
+        listDiv.push({ 'title': title, 'description': description });
     });
-    console.log(listDiv);
+
     return listDiv;
 }
 
-main();
+module.exports = parseHTMLContent;
