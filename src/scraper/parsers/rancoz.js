112bde089b771701c6a79c814213f9b9b5443d25
mrima <mrimamss@gmail.com>
2024-02-16 10:47:00 +0300

    rancoz  ransomware parser

--- src/scraper/parsers/rancoz.js
+++ src/scraper/parsers/rancoz.js
@@ -1,29 +1,17 @@
-const fs = require('fs');
 const cheerio = require('cheerio');
 
-function main() {
+function parseHTMLContent(htmlContent) {
     let listDiv = [];
+    const $ = cheerio.load(htmlContent);
 
-    fs.readdirSync('source').forEach(filename => {
-        try {
-            if (filename.startsWith(__filename.split('.')[0] + '-')) {
-                let htmlDoc = 'source/' + filename;
-                let file = fs.readFileSync(htmlDoc, 'utf8');
-                let $ = cheerio.load(file);
-                let divsName = $('tr.trow');
-                divsName.each((i, div) => {
-                    let title = $(div).find('td').first().text().trim();
-                    let description = $(div).find('td').eq(2).text().trim();
-                    let link = $(div).find('td').eq(5).children().first().attr('href');
-                    listDiv.push({ 'title': title, 'description': description, 'link': link, 'slug': filename });
-                });
-            }
-        } catch (error) {
-            console.log("Failed during : " + filename);
-        }
+    $('tr.trow').each((i, div) => {
+        let title = $(div).find('td').first().text().trim();
+        let description = $(div).find('td').eq(2).text().trim();
+        let link = $(div).find('td').eq(5).children().first().attr('href');
+        listDiv.push({ 'title': title, 'description': description, 'link': link });
     });
-    console.log(listDiv);
+
     return listDiv;
 }
 
-main();
+module.exports = parseHTMLContent;
