7191572dcdfd8c4db95dc747a63fd622ebb96e0a
mrima <mrimamss@gmail.com>
2024-02-16 13:35:56 +0300

    ransomexx ransomware parser

--- src/scraper/parsers/ransomexx.js
+++ src/scraper/parsers/ransomexx.js
@@ -1,33 +1,17 @@
-const fs = require('fs');
-const path = require('path');
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+const cheerio = require('cheerio');
 
-async function main() {
-    let list_div = [];
+function parseHTMLContent(htmlContent) {
+    let listDiv = [];
+    const $ = cheerio.load(htmlContent);
 
-    const files = fs.readdirSync('source');
-    for (let i = 0; i < files.length; i++) {
-        const filename = files[i];
-        try {
-            if (filename.startsWith(path.basename(__filename, '.js') + '-')) {
-                const html_doc = path.join('source', filename);
-                const file = fs.readFileSync(html_doc, 'utf-8');
-                const dom = new JSDOM(file);
-                const divs_name = dom.window.document.querySelectorAll('div.card-body');
-                for (let div of divs_name) {
-                    let title = div.querySelector('h5').textContent.trim();
-                    let description = div.querySelectorAll('p.card-text')[1].textContent.trim();
-                    let link = div.querySelector('a.btn.btn-outline-primary').href;
-                    list_div.push({'title': title, 'description': description, 'link': link, 'slug': filename});
-                }
-            }
-        } catch (error) {
-            console.log("Failed during : " + filename);
-        }
-    }
-    console.log(list_div);
-    return list_div;
+    $('div.card-body').each((i, div) => {
+        const title = $(div).find('h5').text().trim();
+        const description = $(div).find('p.card-text').eq(1).text().trim();
+        const link = $(div).find('a.btn.btn-outline-primary').attr('href');
+        listDiv.push({ 'title': title, 'description': description, 'link': link });
+    });
+
+    return listDiv;
 }
 
-main();
+module.exports = parseHTMLContent;
