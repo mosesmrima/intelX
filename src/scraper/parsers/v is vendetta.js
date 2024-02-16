242adc7cd54c0f9f356c169365fa2f2ad7c35ad9
mrima <mrimamss@gmail.com>
2024-02-16 13:33:15 +0300

    v is vendetta ransomware parser

--- src/scraper/parsers/v is vendetta.js
+++ src/scraper/parsers/v is vendetta.js
@@ -1,29 +1,17 @@
-const fs = require('fs');
 const cheerio = require('cheerio');
 
-function main() {
+function parseHTMLContent(htmlContent) {
     let listDiv = [];
+    const $ = cheerio.load(htmlContent);
 
-    fs.readdirSync('source').forEach(filename => {
-        try {
-            if (filename.startsWith(__filename.split('.')[0] + '-')) {
-                let htmlDoc = 'source/' + filename;
-                let file = fs.readFileSync(htmlDoc, 'utf8');
-                let $ = cheerio.load(file);
-                let divsName = $('div.post');
-                divsName.each((i, div) => {
-                    let title = $(div).find('a').attr('href').split('/')[2];
-                    let description = $(div).find('p').text().trim();
-                    let link = $(div).find('a').attr('href');
-                    listDiv.push({ 'title': title, 'description': description, 'link': link, 'slug': filename });
-                });
-            }
-        } catch (error) {
-            console.log("Failed during : " + filename);
-        }
+    $('div.post').each((i, div) => {
+        const title = $(div).find('a').attr('href').split('/')[2];
+        const description = $(div).find('p').text().trim();
+        const link = $(div).find('a').attr('href');
+        listDiv.push({ 'title': title, 'description': description, 'link': link });
     });
-    console.log(listDiv);
+
     return listDiv;
 }
 
-main();
+module.exports = parseHTMLContent;
