3d663b3d5a0da2faef4b1de0674b1f25f64c3502
mrima <mrimamss@gmail.com>
2024-02-16 10:45:44 +0300

    moses ransomware parser

--- src/scraper/parsers/moses.js
+++ src/scraper/parsers/moses.js
@@ -1,27 +1,16 @@
-const fs = require('fs');
-const path = require('path');
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+const cheerio = require('cheerio');
 
-async function main() {
-    let list_div = [];
+function parseHTMLContent(htmlContent) {
+    let listDiv = [];
+    const $ = cheerio.load(htmlContent);
 
-    const files = fs.readdirSync('source');
-    for (let i = 0; i < files.length; i++) {
-        const filename = files[i];
-        if (filename.startsWith(path.basename(__filename, '.js') + '-')) {
-            const html_doc = path.join('source', filename);
-            const file = fs.readFileSync(html_doc, 'utf-8');
-            const dom = new JSDOM(file);
-            const divs_name = dom.window.document.querySelectorAll('a.elementskit-entry-thumb');
-            for (let div of divs_name) {
-                list_div.push(div.querySelector('img').alt.trim());
-            }
-        }
-    }
-    list_div = [...new Set(list_div)]; // remove duplicates
-    console.log(list_div);
-    return list_div;
+    $('a.elementskit-entry-thumb').each((i, element) => {
+        const imgAlt = $(element).find('img').attr('alt').trim();
+        listDiv.push(imgAlt);
+    });
+
+    listDiv = [...new Set(listDiv)]; // Remove duplicates
+    return listDiv;
 }
 
-main();
+module.exports = parseHTMLContent;
