60c0e96414f34bf6d5ccb2725ae68c01f383d4ea
mrima <mrimamss@gmail.com>
2024-02-16 10:43:50 +0300

    lv ransomware parser

--- src/scraper/parsers/lv.js
+++ src/scraper/parsers/lv.js
@@ -1,36 +1,29 @@
-const fs = require('fs');
-const path = require('path');
 const cheerio = require('cheerio');
 
-function main() {
-    let list_div = [];
-    const directoryPath = path.join(__dirname, 'source');
-    fs.readdir(directoryPath, function (err, files) {
-        if (err) {
-            return console.log('Unable to scan directory: ' + err);
-        } 
-        files.forEach(function (filename) {
-            if(filename.startsWith(path.basename(__filename).split('.')[0]+'-')) {
-                console.log(filename);
-                let data = fs.readFileSync(path.join(directoryPath, filename));
-                let $ = cheerio.load(data);
-                if (filename.includes('api')) {
-                    let jsonpart = $('pre').text();
-                    let data = JSON.parse(jsonpart);
-                    for (let entry of data['posts']) {
-                        list_div.push(entry['title'].trim());
-                    }
-                } else {
-                    let divs_name = $('div.blog-post.blog-main.posts_at_first');
-                    divs_name.each((i, div) => {
-                        list_div.push($(div).find('h2 a').text().trim());
-                    });
-                }
-            }
+function parseHTMLContent(htmlContent, isAPI) {
+    let listDiv = [];
+    const $ = cheerio.load(htmlContent);
+
+    if (isAPI) {
+        try {
+            const jsonPart = $('pre').text();
+            const data = JSON.parse(jsonPart);
+            data['posts'].forEach(entry => {
+                listDiv.push(entry['title'].trim());
+            });
+        } catch (error) {
+            console.error('Error parsing JSON:', error);
+        }
+    } else {
+        $('div.blog-post.blog-main.posts_at_first').each((i, div) => {
+            const title = $(div).find('h2 a').text().trim();
+            listDiv.push(title);
         });
-        list_div = [...new Set(list_div)];
-        console.log(list_div);
-    });
+    }
+
+    listDiv = [...new Set(listDiv)];
+    return listDiv;
 }
 
-main();
+module.exports = parseHTMLContent;
+
