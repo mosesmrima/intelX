7f63db49c590f495f2d008c2c031dd004c828f4e
mrima <mrimamss@gmail.com>
2024-02-16 10:43:27 +0300

    lorenz ransomware parser

--- src/scraper/parsers/lorenz.js
+++ src/scraper/parsers/lorenz.js
@@ -1,31 +1,18 @@
-const fs = require('fs');
-const path = require('path');
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+const cheerio = require('cheerio');
 
-async function main() {
-    let list_div = [];
+function parseHTMLContent(htmlContent) {
+    let listDiv = [];
+    const $ = cheerio.load(htmlContent);
+    const divsName = $('div.panel-heading');
 
-    const files = fs.readdirSync('source');
-    for (let i = 0; i < files.length; i++) {
-        const filename = files[i];
-        if (filename.startsWith(path.basename(__filename).split('.')[0]+'-')) {
-            const html_doc = 'source/'+filename;
-            const data = fs.readFileSync(html_doc, 'utf8');
-            const dom = new JSDOM(data);
-            const document = dom.window.document;
-            const divs_name = document.querySelectorAll('div.panel-heading');
-            divs_name.forEach(div => {
-                const items = div.querySelectorAll('h3');
-                items.forEach(item => {
-                    list_div.push(item.textContent.trim());
-                });
-            });
-        }
-    }
-    list_div = [...new Set(list_div)];
-    console.log(list_div);
-    return list_div;
+    divsName.each((index, div) => {
+        $(div).find('h3').each((idx, item) => {
+            listDiv.push($(item).text().trim());
+        });
+    });
+
+    listDiv = [...new Set(listDiv)]; // Remove duplicates
+    return listDiv;
 }
 
-main();
+module.exports = parseHTMLContent;
