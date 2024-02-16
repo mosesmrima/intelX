ae6299773782bb6d9ea54a033c894f7761e562b2
mrima <mrimamss@gmail.com>
2024-02-16 10:46:52 +0300

    ragnarlocker  ransomware parser

--- src/scraper/parsers/ragnarlocker.js
+++ src/scraper/parsers/ragnarlocker.js
@@ -1,33 +1,21 @@
-const fs = require('fs');
-const path = require('path');
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+const cheerio = require('cheerio');
 
-async function main() {
-    let list_div = [];
+function parseHTMLContent(htmlContent) {
+    let listDiv = [];
 
-    const files = fs.readdirSync('source');
-    for (let i = 0; i < files.length; i++) {
-        const filename = files[i];
-        if (filename.startsWith(path.basename(__filename).split('.')[0]+'-')) {
-            const html_doc = 'source/'+filename;
-            const data = fs.readFileSync(html_doc, 'utf8');
-            const dom = new JSDOM(data);
-            const document = dom.window.document;
-            const divs_name = document.querySelectorAll('div.card');
-            divs_name.forEach(div => {
-                const items = div.querySelectorAll('a');
-                items.forEach(item => {
-                    const title = item.textContent.trim();
-                    const description = "";
-                    const link = item.href;
-                    list_div.push({ "title": title, "description": description, "link": link, "slug": filename});
-                });
-            });
-        }
-    }
-    console.log(list_div);
-    return list_div;
+    const $ = cheerio.load(htmlContent);
+
+    $('div.card').each((index, div) => {
+        $(div).find('a').each((i, item) => {
+            const title = $(item).text().trim();
+            const description = "";
+            const link = $(item).attr('href');
+            listDiv.push({ "title": title, "description": description, "link": link });
+        });
+    });
+
+    return listDiv;
 }
 
-main();
+module.exports = parseHTMLContent;
+
