e4df1531644c84b95f7d6c961925bc5e6788b87c
mrima <mrimamss@gmail.com>
2024-02-16 15:35:50 +0300

    vsop ransomware parser

--- src/scraper/parsers/vsop.js
+++ src/scraper/parsers/vsop.js
@@ -1,35 +1,18 @@
-const fs = require('fs');
-const path = require('path');
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+const cheerio = require('cheerio');
 
-async function main() {
-    let list_div = [];
+function parseHTMLContent(htmlContent) {
+    let listDiv = [];
+    const $ = cheerio.load(htmlContent);
 
-    const files = fs.readdirSync('source');
-    for (let i = 0; i < files.length; i++) {
-        const filename = files[i];
-        try {
-            if (filename.startsWith(path.basename(__filename, '.js') + '-')) {
-                const html_doc = path.join('source', filename);
-                const file = fs.readFileSync(html_doc, 'utf-8');
-                const dom = new JSDOM(file);
-                const divs_name = dom.window.document.querySelectorAll('div.group.center.testimonials');
-                for (let div of divs_name) {
-                    let articles = div.querySelectorAll('article');
-                    for (let article of articles) {
-                        let title = article.querySelector('h6').textContent.trim();
-                        let description = article.querySelector('blockquote').textContent.trim();
-                        list_div.push({'title': title, 'description': description});
-                    }
-                }
-            }
-        } catch (error) {
-            console.log("Failed during : " + filename);
-        }
-    }
-    console.log(list_div);
-    return list_div;
+    $('div.group.center.testimonials').each((i, div) => {
+        $(div).find('article').each((j, article) => {
+            const title = $(article).find('h6').text().trim();
+            const description = $(article).find('blockquote').text().trim();
+            listDiv.push({ 'title': title, 'description': description });
+        });
+    });
+
+    return listDiv;
 }
 
-main();
+module.exports = parseHTMLContent;
