4550488949f3ba76ce7b576764b1270202059682
mrima <mrimamss@gmail.com>
2024-02-16 13:34:39 +0300

    rhysida ransomware parser

--- src/scraper/parsers/rhysida.js
+++ src/scraper/parsers/rhysida.js
@@ -1,37 +1,21 @@
-const fs = require('fs');
-const axios = require('axios');
 const cheerio = require('cheerio');
 
-async function main() {
-    let list_div = [];
+function parseHTMLContent(htmlContent) {
+    let listDiv = [];
+    const $ = cheerio.load(htmlContent);
 
-    const files = fs.readdirSync('source');
-    for (let i = 0; i < files.length; i++) {
-        const filename = files[i];
-        if (filename.startsWith(__filename.split('.')[0] + '-')) {
-            try {
-                const html_doc = 'source/' + filename;
-                const data = fs.readFileSync(html_doc, 'utf8');
-                const $ = cheerio.load(data);
-                
-                const divs_name = $('div.border.m-2.p-2');
-                divs_name.each((index, element) => {
-                    const title = $(element).find('div.m-2.h4').find('a').text().trim();
-                    const description = $(element).find('div.m-2').text().trim();
-                    try {
-                        const link = 'archive.php?company=' + $(element).find('button').attr('data-company');
-                        list_div.push({ "title": title, "description": description, "link": link, "slug": filename });
-                    } catch (error) {
-                        list_div.push({ "title": title, "description": description });
-                    }
-                });
-            } catch (error) {
-                console.log("Failed during : " + filename);
-            }
+    $('div.border.m-2.p-2').each((index, element) => {
+        const title = $(element).find('div.m-2.h4').find('a').text().trim();
+        const description = $(element).find('div.m-2').text().trim();
+        try {
+            const link = 'archive.php?company=' + $(element).find('button').attr('data-company');
+            listDiv.push({ "title": title, "description": description, "link": link });
+        } catch (error) {
+          listDiv.push({ "title": title, "description": description });
         }
-    }
-    console.log(list_div);
-    return list_div;
+    });
+
+    return listDiv;
 }
 
-main();
+module.exports = parseHTMLContent;
