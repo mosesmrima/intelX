ba9a5660f9d2649881a6794d06b23fab1c26ffab
mrima <mrimamss@gmail.com>
2024-02-16 10:41:31 +0300

    everest ransomware parser

--- src/scraper/parsers/everest.js
+++ src/scraper/parsers/everest.js
@@ -1,29 +1,16 @@
-const fs = require('fs');
 const cheerio = require('cheerio');
 
-function main() {
+function parseHTMLContent(htmlContent) {
     let listDiv = [];
-
-    fs.readdirSync('source').forEach(filename => {
-        try {
-            if (filename.startsWith(__filename.split('.')[0]+'-')) {
-                let htmlDoc = 'source/' + filename;
-                let file = fs.readFileSync(htmlDoc, 'utf8');
-                let $ = cheerio.load(file);
-                let divsName = $('article');
-                divsName.each((i, div) => {
-                    let title = $(div).find('h2.entry-title.heading-size-1 a').text().trim();
-                    let description = $(div).find('div.entry-content p').text().trim();
-                    let link = $(div).find('h2.entry-title.heading-size-1 a').attr('href');
-                    listDiv.push({title: title, description: description, link: link, slug: filename});
-                });
-            }
-        } catch (error) {
-            console.log("Failed during : " + filename);
-        }
+    const $ = cheerio.load(htmlContent);
+    const divsName = $('article');
+    divsName.each((i, div) => {
+        let title = $(div).find('h2.entry-title.heading-size-1 a').text().trim();
+        let description = $(div).find('div.entry-content p').first().text().trim(); // Assuming first paragraph is desired
+        let link = $(div).find('h2.entry-title.heading-size-1 a').attr('href');
+        listDiv.push({title: title, description: description, link: link});
     });
-    console.log(listDiv);
     return listDiv;
 }
 
-main();
+module.exports = parseHTMLContent;
