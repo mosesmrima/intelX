c6a40b24c676cee41fdf035ba025951080257536
mrima <mrimamss@gmail.com>
2024-02-16 10:40:24 +0300

    dark power group parser

--- src/scraper/parsers/dark power.js
+++ src/scraper/parsers/dark power.js
@@ -1,28 +1,15 @@
-const fs = require('fs');
 const cheerio = require('cheerio');
 
-function main() {
-    let list_div = [];
-
-    fs.readdirSync('source').forEach(filename => {
-        try {
-            if (filename.startsWith(__filename.split('.')[0]+'-')) {
-                let html_doc = 'source/'+filename;
-                let file = fs.readFileSync(html_doc, 'utf8');
-                let $ = cheerio.load(file);
-                let divs_name = $('div.sm\\:w-1\\/2.mb-10.px-4');
-                divs_name.each((i, div) => {
-                    let title = $(div).find('h2').text().trim();
-                    let description = '';
-                    list_div.push({'title': title, 'description': description});
-                });
-            }
-        } catch (error) {
-            console.log("Failed during : " + filename);
-        }
+function parseHTMLContent(htmlContent) {
+    let listDiv = [];
+    const $ = cheerio.load(htmlContent);
+    const divsName = $('div[class="sm:w-1/2 mb-10 px-4"]');
+    divsName.each((i, div) => {
+        let title = $(div).find('h2').text().trim();
+        let description = '';
+        listDiv.push({'title': title, 'description': description});
     });
-    console.log(list_div);
-    return list_div;
+    return listDiv;
 }
 
-main();
+module.exports = parseHTMLContent;
