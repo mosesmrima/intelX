9ef5c6a3162173f3385702e367a907a77985ca9a
mrima <mrimamss@gmail.com>
2024-02-16 10:44:10 +0300

    mallox ransomware parser

--- src/scraper/parsers/mallox.js
+++ src/scraper/parsers/mallox.js
@@ -1,38 +1,23 @@
-const fs = require('fs');
 const cheerio = require('cheerio');
-const path = require('path');
 
-function main() {
-    let list_div = [];
+function parseHTMLContent(htmlContent) {
+    let listDiv = [];
+    const $ = cheerio.load(htmlContent);
+    const divsName = $('div.card.mb-4.box-shadow');
 
-    fs.readdirSync('source').forEach(filename => {
-        try {
-            if (filename.startsWith(path.basename(__filename, '.js') + '-')) {
-                let html_doc = 'source/' + filename;
-                let file = fs.readFileSync(html_doc, 'utf8');
-                let $ = cheerio.load(file);
-                let divs_name = $('div.card.mb-4.box-shadow');
-                divs_name.each((i, div) => {
-                    let title = $(div).find('h4.card-title').text().trim();
-                    let description = '';
-                    $(div).find('p').each((j, p) => {
-                        description += $(p).text() + '\n';
-                    });
-                    let linktmp = $(div).find('a.btn.btn-primary.btn-sm');
-                    if (linktmp != null) {
-                        let link = linktmp.attr('href');
-                        list_div.push({"title" : title, "description" : description, "link": link, "slug": filename});
-                    } else {
-                        list_div.push({"title" : title, "description" : description});
-                    }
-                });
-            }
-        } catch (err) {
-            console.log("Failed during : " + filename);
-        }
+    divsName.each((i, div) => {
+        let title = $(div).find('h4.card-title').text().trim();
+        let description = '';
+        $(div).find('p').each((j, p) => {
+            description += $(p).text().trim() + '\n';
+        }).trim();
+        let linkElement = $(div).find('a.btn.btn-primary.btn-sm');
+        let link = linkElement.attr('href') ? linkElement.attr('href') : '';
+
+        listDiv.push({"title": title, "description": description.trim(), "link": link});
     });
-    console.log(list_div);
-    return list_div;
+
+    return listDiv;
 }
 
-main();
+module.exports = parseHTMLContent;
