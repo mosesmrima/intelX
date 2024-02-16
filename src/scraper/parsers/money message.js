524bdf219540adaa15c102d59973a675fb52855b
mrima <mrimamss@gmail.com>
2024-02-16 10:45:20 +0300

    money message ransomware parser

--- src/scraper/parsers/money message.js
+++ src/scraper/parsers/money message.js
@@ -1,35 +1,22 @@
-const fs = require('fs');
-const path = require('path');
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+const cheerio = require('cheerio');
 
-async function main() {
-    let list_div = [];
+function parseHTMLContent(htmlContent) {
+    let listDiv = [];
+    const $ = cheerio.load(htmlContent);
 
-    const files = fs.readdirSync('source');
-    for (let i = 0; i < files.length; i++) {
-        const filename = files[i];
-        if (filename.startsWith(path.basename(__filename).split('.')[0]+'-')) {
-            const html_doc = 'source/'+filename;
-            const data = fs.readFileSync(html_doc, 'utf8');
-            const dom = new JSDOM(data);
-            const document = dom.window.document;
-            const divs_name = document.querySelectorAll('a');
-            divs_name.forEach(div => {
-                try {
-                    const section = div.querySelector('div.MuiBox-root.css-0');
-                    const title = section.textContent.trim();
-                    const description = "";
-                    const link = div.href;
-                    list_div.push({"title" : title, "description" : description, "link": link, "slug": filename});
-                } catch (error) {
-                    console.log("Failed during : " + filename);
-                }
-            });
+    $('a').each((i, elem) => {
+        try {
+            const section = $(elem).find('div.MuiBox-root.css-0');
+            const title = section.text().trim();
+            const description = "";
+            const link = $(elem).attr('href');
+            listDiv.push({"title": title, "description": description, "link": link});
+        } catch (error) {
+            console.error("Error parsing element:", error);
         }
-    }
-    console.log(list_div);
-    return list_div;
+    });
+
+    return listDiv;
 }
 
-main();
+module.exports = parseHTMLContent;
