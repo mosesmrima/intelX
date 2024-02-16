3003ad95f57f1701be57a0517dcc30b39ce62ece
mrima <mrimamss@gmail.com>
2024-02-16 10:46:42 +0300

    ragroup  ransomware parser

--- src/scraper/parsers/ra group.js
+++ src/scraper/parsers/ra group.js
@@ -1,35 +1,19 @@
-const fs = require('fs');
-const axios = require('axios');
 const cheerio = require('cheerio');
 
-async function main() {
-    let list_div = [];
+function parseHTMLContent(htmlContent) {
+    let listDiv = [];
+    const $ = cheerio.load(htmlContent);
 
-    const files = fs.readdirSync('source');
-    for (let i = 0; i < files.length; i++) {
-        const filename = files[i];
-        if (filename.startsWith(__filename.split('.')[0] + '-')) {
-            try {
-                const html_doc = 'source/' + filename;
-                const data = fs.readFileSync(html_doc, 'utf8');
-                const $ = cheerio.load(data);
-                
-                const divs_name = $('div.row');
-                divs_name.each((index, element) => {
-                    $(element).find('a').each((i, item) => {
-                        const title = $(item).text().trim();
-                        const description = '';
-                        const link = $(item).attr('href');
-                        list_div.push({ "title": title, "description": description, "link": link, "slug": filename });
-                    });
-                });
-            } catch (error) {
-                console.log("Failed during : " + filename);
-            }
-        }
-    }
-    console.log(list_div);
-    return list_div;
+    $('div.row').each((index, element) => {
+        $(element).find('a').each((i, item) => {
+            const title = $(item).text().trim();
+            const description = ''; // Keeping description empty as per original logic
+            const link = $(item).attr('href');
+            listDiv.push({ "title": title, "description": description, "link": link });
+        });
+    });
+
+    return listDiv;
 }
 
-main();
+module.exports = parseHTMLContent;
