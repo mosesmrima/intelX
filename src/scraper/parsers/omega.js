d23c760d23469a695cd124dfa6fbe76e5960eba1
mrima <mrimamss@gmail.com>
2024-02-16 10:46:08 +0300

    omega ransomware parser

--- src/scraper/parsers/omega.js
+++ src/scraper/parsers/omega.js
@@ -1,31 +1,22 @@
-const fs = require('fs');
 const cheerio = require('cheerio');
-const path = require('path');
 
-function main() {
-    let list_div = [];
+function parseHTMLContent(htmlContent) {
+    let listDiv = [];
+    const $ = cheerio.load(htmlContent);
 
-    fs.readdirSync('source').forEach(filename => {
-        try {
-            if (filename.startsWith(path.basename(__filename, '.js') + '-')) {
-                let html_doc = 'source/' + filename;
-                let data = fs.readFileSync(html_doc, 'utf8');
-                let $ = cheerio.load(data);
-                let divs_name = $('tr.trow');
-                divs_name.each((i, div) => {
-                    let item = $(div).find('td');
-                    let title = $(item[0]).text().trim();
-                    let description = $(item[2]).text().trim();
-                    let link = $(item[5]).find('a').attr('href');
-                    list_div.push({ 'title': title, 'description': description, 'link': link, 'slug': filename });
-                });
-            }
-        } catch (error) {
-            console.log("Failed during : " + filename);
-        }
+    $('tr.trow').each((i, div) => {
+        let item = $(div).find('td');
+        let title = $(item[0]).text().trim();
+        let description = $(item[2]).text().trim();
+        let link = $(item[5]).find('a').attr('href');
+        listDiv.push({ 'title': title, 'description': description, 'link': link });
     });
-    console.log(list_div);
-    return list_div;
+
+    return listDiv;
 }
 
-main();
+module.exports = parseHTMLContent;
+
+// Example usage:
+// Assuming you have the HTML content in `htmlContent` variable
+// const html
