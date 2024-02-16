34a617ed29f808a29f216279dd01e06bc9b1a1a7
mrima <mrimamss@gmail.com>
2024-02-16 10:45:08 +0300

    metaencryptor ransomware parser

--- src/scraper/parsers/metaencryptor.js
+++ src/scraper/parsers/metaencryptor.js
@@ -1,32 +1,21 @@
-const fs = require('fs');
-const axios = require('axios');
 const cheerio = require('cheerio');
 
-async function main() {
-    let list_div = [];
+async function parseHTMLContent(htmlContent) {
+    let listDiv = [];
+    try {
+        const $ = cheerio.load(htmlContent);
+        const divsName = $('div.card.shadow-sm.border-info.shadow-lg');
 
-    const files = fs.readdirSync('source');
-    for (let i = 0; i < files.length; i++) {
-        const filename = files[i];
-        if (filename.startsWith(__filename.split('.')[0] + '-')) {
-            try {
-                const html_doc = 'source/' + filename;
-                const data = fs.readFileSync(html_doc, 'utf8');
-                const $ = cheerio.load(data);
-                const divs_name = $('div.card.shadow-sm.border-info.shadow-lg');
-                divs_name.each((index, element) => {
-                    const title = $(element).find('div.card-header').text().trim();
-                    const description = $(element).find('p.card-text').text().trim();
-                    const link = $(element).find('a.btn.btn-primary.btn-sm').attr('href');
-                    list_div.push({ "title": title, "description": description, 'link': link, 'slug': filename });
-                });
-            } catch (error) {
-                console.log("Failed during : " + filename);
-            }
-        }
+        divsName.each((index, element) => {
+            const title = $(element).find('div.card-header').text().trim();
+            const description = $(element).find('p.card-text').text().trim();
+            const link = $(element).find('a.btn.btn-primary.btn-sm').attr('href');
+            listDiv.push({"title": title, "description": description, 'link': link});
+        });
+    } catch (error) {
+        console.error("Error parsing HTML content:", error);
     }
-    console.log(list_div);
-    return list_div;
+    return listDiv;
 }
 
-main();
+module.exports = parseHTMLContent;
