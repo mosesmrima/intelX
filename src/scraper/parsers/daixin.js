4607796848eb1f752772dcfc4ffad06189b6d8e7
mrima <mrimamss@gmail.com>
2024-02-16 10:40:09 +0300

    daixin group parser

--- src/scraper/parsers/daixin.js
+++ src/scraper/parsers/daixin.js
@@ -1,29 +1,15 @@
-const fs = require('fs');
 const cheerio = require('cheerio');
-const path = require('path');
 
-function main() {
-    let list_div = [];
-
-    fs.readdirSync('source').forEach(filename => {
-        try {
-            if (filename.startsWith(path.basename(__filename, '.js') + '-')) {
-                let html_doc = 'source/' + filename;
-                let file = fs.readFileSync(html_doc, 'utf8');
-                let $ = cheerio.load(file);
-                let divs_name = $('div.border.border-warning.card-body.shadow-lg');
-                divs_name.each((i, div) => {
-                    let title = $(div).find('h4').text().trim();
-                    let description = $(div).find('p').text().trim();
-                    list_div.push({'title':title, 'description': description});
-                });
-            }
-        } catch (err) {
-            console.log("Failed during : " + filename);
-        }
+function parseHTMLContent(htmlContent) {
+    let listDiv = [];
+    const $ = cheerio.load(htmlContent);
+    const divsName = $('div.border.border-warning.card-body.shadow-lg');
+    divsName.each((i, div) => {
+        let title = $(div).find('h4').text().trim();
+        let description = $(div).find('p').text().trim();
+        listDiv.push({'title': title, 'description': description});
     });
-    console.log(list_div);
-    return list_div;
+    return listDiv;
 }
 
-main();
+module.exports = parseHTMLContent;
