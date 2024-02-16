b1f01f0469fcfdbb91a40cb47d0aa98789abb0bd
mrima <mrimamss@gmail.com>
2024-02-16 10:41:15 +0300

    dunghill ransomware group parser

--- src/scraper/parsers/dunghill.js
+++ src/scraper/parsers/dunghill.js
@@ -1,29 +1,23 @@
-const fs = require('fs');
 const cheerio = require('cheerio');
 
-function main() {
-    let list_div = [];
-
-    fs.readdirSync('source').forEach(filename => {
-        try {
-            if (filename.startsWith(__filename.split('.')[0]+'-')) {
-                let html_doc = 'source/'+filename;
-                let file = fs.readFileSync(html_doc, 'utf8');
-                let $ = cheerio.load(file);
-                let divs = $('div.custom-container');
-                divs.each((i, div) => {
-                    let title = $(div).find('div.ibody_title').text().trim();
-                    let description = $(div).find('div.ibody_body').find('p').eq(2).text().trim();
-                    let link = $(div).find('div.ibody_ft_right').find('a').attr('href');
-                    list_div.push({'title': title, 'description': description, 'link': link, 'slug': filename});
-                });
-            }
-        } catch (error) {
-            console.log("Failed during : " + filename);
-        }
+function parseHTMLContent(htmlContent) {
+    let listDiv = [];
+    const $ = cheerio.load(htmlContent);
+    // Process divs with class "custom-container"
+    $('div.custom-container').each((i, div) => {
+        let title = $(div).find('div.ibody_title').text().trim();
+        let description = $(div).find('div.ibody_body').find('p').eq(2).text().trim();
+        let link = $(div).find('div.ibody_ft_right').find('a').attr('href');
+        listDiv.push({'title': title, 'description': description, 'link': link});
     });
-    console.log(list_div);
-    return list_div;
+    // Process divs with class "custom-container2" if needed
+    $('div.custom-container2').each((i, div) => {
+        let title = $(div).find('div.ibody_title').text().trim();
+        let description = $(div).find('div.ibody_body').find('p').eq(2).text().trim();
+        let link = $(div).find('div.ibody_ft_right').find('a').attr('href');
+        listDiv.push({'title': title, 'description': description, 'link': link});
+    });
+    return listDiv;
 }
 
-main();
+module.exports = parseHTMLContent;
